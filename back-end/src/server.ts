ef7666f37855c059b15bbc1c1867043bf15941c1
luandassis <14314403616@bellinatiperez.com.br>
2024-02-11 23:20:08 -0300

    fix(api): registers errorHandler middleware to the app middlewares and corrects pagination request error name

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { router } from "./routes/router";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
