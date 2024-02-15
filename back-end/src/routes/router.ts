4a3cab1a9ea6e8cb7647624441673c4432d04e70
luandassis <14314403616@bellinatiperez.com.br>
2024-02-15 02:27:44 -0300

    fix(api): add cors middleware to the api to fix cors requests errors

--- back-end/src/routes/router.ts
+++ back-end/src/routes/router.ts
@@ -2,6 +2,7 @@ import { NextFunction, Router, Request, Response } from "express";
 import { StudentsService } from "../services/StudentsService";
 import { StudentsRepository } from "../repositories/students/StudentsRepository";
 import { StudentsController } from "../controllers/students/StudentsController";
+import cors from "cors";
 
 const router = Router();
 
@@ -9,23 +10,23 @@ const studentsRepository = new StudentsRepository();
 const studentsService = new StudentsService(studentsRepository);
 const studentsController = new StudentsController(studentsService);
 
-router.post("/students", async (req: Request, res: Response, next: NextFunction) => {
+router.post("/students", cors(), async (req: Request, res: Response, next: NextFunction) => {
   studentsController.createStudent(req, res, next);
 });
 
-router.get("/students", async (req: Request, res: Response, next: NextFunction) => {
+router.get("/students", cors(), async (req: Request, res: Response, next: NextFunction) => {
   studentsController.getStudents(req, res, next);
 });
 
-router.get("/students/:ra", async (req: Request, res: Response, next: NextFunction) => {
+router.get("/students/:ra", cors(), async (req: Request, res: Response, next: NextFunction) => {
   studentsController.getStudent(req, res, next);
 });
 
-router.put("/students/:ra", async (req: Request, res: Response, next: NextFunction) => {
+router.put("/students/:ra", cors(), async (req: Request, res: Response, next: NextFunction) => {
   studentsController.updateStudent(req, res, next);
 });
 
-router.delete("/students/:ra", async (req: Request, res: Response, next: NextFunction) => {
+router.delete("/students/:ra", cors(), async (req: Request, res: Response, next: NextFunction) => {
   studentsController.deleteStudent(req, res, next);
 });
 
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { router } from "./routes/router";
 import { errorHandler } from "./middlewares/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use(errorHandler);
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
